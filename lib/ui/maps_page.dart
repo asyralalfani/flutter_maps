c3b16431ac92bfcf5b408d80b2fb42c3ed96189a
Asyral <asyral777@gmail.com>
2021-01-02 19:08:35 +0700

    first commit

---
+++ lib/ui/maps_page.dart
@@ -0,0 +1,15 @@
+part of 'pages.dart';
+
+class MapsPage extends StatefulWidget {
+  @override
+  _MapsPageState createState() => _MapsPageState();
+}
+
+class _MapsPageState extends State<MapsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/ui/pages.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+part 'maps_page.dart';
